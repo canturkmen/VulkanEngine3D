d188231b91e81b3ce16ebda149e28d83171926a6
Mehmet Can Turkmen <turkmenc098@gmail.com>
2024-08-20 13:09:13 +0300

    Implemented basic linear algebra functions.

---
+++ engine/src/math/math_types.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "defines.h"
+
+typedef union vec2_u
+{
+    // An array of x, y.
+    f32 elements[2];
+
+    struct 
+    {
+        union 
+        {
+            // The first element.
+            f32 x, r, s, u;
+        };
+
+        union 
+        {
+            // The second element.
+            f32 y, g, t, v;
+        };
+    };
+
+} vec2;
+
+typedef struct vec3_u
+{
+    union 
+    {
+        f32 elements[3];
+        struct
+        {
+            union 
+            {
+                // The first element.
+                f32 x, r, s, u;
+            };
+
+            union 
+            {
+                // The second element.
+                f32 y, g, t, v;
+            };
+
+            union 
+            {
+                // The third element.
+                f32 z, b, p, w;
+            };
+        };   
+    };
+} vec3;
+
+typedef union vec4_u
+{
+#if defined(VEUSE_SIMD)
+    // Used for SIMD operations.
+    alignas(16) __m128 data;
+#endif
+    // An array of x, y, z and w.
+    alignas(16) f32 elements[4];
+    union
+    {
+        struct
+        {
+            union 
+            {
+                // The first element.
+                f32 x, r, s;
+            };
+
+            union 
+            {
+                // The second element.
+                f32 y, g, t;
+            };
+
+            union 
+            {
+                // The third element.
+                f32 z, b, p;
+            };
+
+            union 
+            {
+                // The fourth element.
+                f32 w, a, q;
+            };
+        };
+    };
+} vec4;
+
+typedef vec4 quat;
