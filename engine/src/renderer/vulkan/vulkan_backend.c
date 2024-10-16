1de830aca1f79310f098edcda80245c043f484a1
Mehmet Can Turkmen <turkmenc098@gmail.com>
2024-10-16 22:14:13 +0300

    Added uniform buffers and descriptor sets.

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -38,6 +38,7 @@ VKAPI_ATTR VkBool32 VKAPI_CALL vk_debug_callback(
 i32 find_memory_index(u32 type_filter, u32 property_flags);
 b8 create_buffers(vulkan_context* context);
 
+
 void create_command_buffers(renderer_backend* backend);
 void regenerate_framebuffers(renderer_backend* backend, vulkan_swapchain* swapchain, vulkan_renderpass* renderpass);
 b8 recreate_swapchain(renderer_backend* backend);
@@ -241,17 +242,19 @@ b8 vulkan_renderer_backend_initialize(struct renderer_backend* backend, const ch
     vertex_3d verts[vert_count];
     vezero_memory(verts, sizeof(vertex_3d) * vert_count);
 
-    verts[0].position.x = 0.0;
-    verts[0].position.y = -0.5;
+    const f32 f = 10.0f;
 
-    verts[1].position.x = 0.5;
-    verts[1].position.y = 0.5;
+    verts[0].position.x = -0.5 * f;
+    verts[0].position.y = -0.5 * f;
 
-    verts[2].position.x = 0;
-    verts[2].position.y = 0.5;
+    verts[1].position.x = 0.5 * f;
+    verts[1].position.y = 0.5 * f;
 
-    verts[3].position.x = 0.5;
-    verts[3].position.y = -0.5;
+    verts[2].position.x = -0.5 * f;
+    verts[2].position.y = 0.5 * f;
+
+    verts[3].position.x = 0.5 * f;
+    verts[3].position.y = -0.5 * f;
 
     const u32 index_count = 6;
     u32 indices[index_count] = {0, 1, 2, 0, 3, 1};
@@ -458,7 +461,22 @@ b8 vulkan_renderer_backend_begin_frame(struct renderer_backend* backend, f32 del
         command_buffer,
         &context.main_renderpass,
         context.swapchain.framebuffers[context.image_index].handle);
-        
+
+    return true;
+}
+
+void vulkan_renderer_update_global_state(mat4 projection, mat4 view, vec3 view_position, vec4 ambient_color, i32 mode)
+{
+    vulkan_command_buffer* command_buffer = &context.graphics_command_buffers[context.image_index];
+
+    vulkan_object_shader_use(&context, &context.object_shader);
+
+    context.object_shader.global_ubo.projection = projection;
+    context.object_shader.global_ubo.view = view;
+
+    // TODO: Other UBO properties.
+    vulkan_object_shader_update_global_state(&context, &context.object_shader);
+
     // TODO: Temporary test code.
     vulkan_object_shader_use(&context, &context.object_shader);
 
@@ -471,10 +489,7 @@ b8 vulkan_renderer_backend_begin_frame(struct renderer_backend* backend, f32 del
 
     // Issue the draw call.
     vkCmdDrawIndexed(command_buffer->handle, 6, 1, 0, 0, 0);
-
     // TODO: End temporary test code.
-
-    return true;
 }
 
 b8 vulkan_renderer_backend_end_frame(struct renderer_backend* backend, f32 delta_time)
